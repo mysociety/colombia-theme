48f75e985b21113d8970f9cf75ef82751d56aab0
Gareth Rees <gareth@garethrees.co.uk>
2018-08-22 15:00:12 +0100

    Override IncomingMessage#parse_raw_email!
    Taken from 0.25.0.24 in preparation for patching in the next commit.

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -5,4 +5,31 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
+  IncomingMessage.class_eval do
+    def parse_raw_email!(force = nil)
+      # The following fields may be absent; we treat them as cached
+      # values in case we want to regenerate them (due to mail
+      # parsing bugs, etc).
+      if self.raw_email.nil?
+        raise "Incoming message id=#{id} has no raw_email"
+      end
+      if (!force.nil? || self.last_parsed.nil?)
+        ActiveRecord::Base.transaction do
+          self.extract_attachments!
+          write_attribute(:sent_at, self.mail.date || self.created_at)
+          write_attribute(:subject, self.mail.subject)
+          write_attribute(:mail_from, MailHandler.get_from_name(self.mail))
+          if self.from_email
+            self.mail_from_domain = PublicBody.extract_domain_from_email(self.from_email)
+          else
+            self.mail_from_domain = ""
+          end
+          write_attribute(:valid_to_reply_to, self._calculate_valid_to_reply_to)
+          self.last_parsed = Time.now
+          self.foi_attachments reload=true
+          self.save!
+        end
+      end
+    end
+  end
 end
